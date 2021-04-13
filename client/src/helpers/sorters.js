16f6cb75cf5e677fc11375fba40f8de56f540325
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-04-13 18:32:27 -0400

    start researching async tests

--- client/src/helpers/sorters.js
+++ client/src/helpers/sorters.js
@@ -1,7 +1,17 @@
-export function bubbleSort(list) {
+export function bubbleSort(list, speed) {
   const parsedArray = [...list]
   
-  return []
+  for(let i = 0; i < parsedArray.length; i++) {
+    for(let j=0; j < parsedArray.length - i; j++) {
+      if(parsedArray[j] > parsedArray[j+1]) {
+        let tmp = parsedArray[j+1]
+        parsedArray[j+1] = parsedArray[j]
+        parsedArray[j] = tmp
+      }
+      await new Promise(r => setTimeout(r, speed));
+    }
+  }
+  return parsedArray
 }
 
 export function quickSort(list) {
--- client/src/helpers/sorters.test.js
+++ client/src/helpers/sorters.test.js
@@ -2,3 +2,29 @@ import { bubbleSort, quickSort, mergeSort } from '../helpers/sorters'
 
 import { renderHook, act } from "@testing-library/react-hooks";
 
+const list = [70, 54, 21, 60, 7, 12, 82, 37, 45, 63, 80, 14]
+
+let speed = 700
+
+// bubbleSort
+test("bubbleSort returns an array", () => {
+  const result = bubbleSort(list);
+  expect(Array.isArray(result)).toBe(true);
+});
+
+test("bubbleSort returns a sorted array in ascending order", () => {
+  const result = bubbleSort(list);
+  expect(result).toEqual(list.sort((a,b) => a - b));
+});
+
+// quickSort
+test("quickSort returns an array", () => {
+  const result = quickSort(list);
+  expect(Array.isArray(result)).toBe(true);
+});
+
+// mergeSort
+test("mergeSort returns an array", () => {
+  const result = mergeSort(list);
+  expect(Array.isArray(result)).toBe(true);
+});
